b4560fb966d780a6c0f3523374089c899d36eb33
Huiseon Yi <heesun0514@gmail.com>
2026-04-19 15:11:28 +0100

    follow instruction https://www.youtube.com/watch?v=Cv0LdP_B5aI

--- backend/database.js
+++ backend/database.js
@@ -1,4 +1,31 @@
 
 //https://www.youtube.com/watch?v=Cv0LdP_B5aI
 
+/*
+"I used SQLite because it’s a serverless database.
+ It’s perfect for this prototype because it ensures data integrity 
+ (like keeping child records organized) while staying lightweight.
+  It makes the app easier to deploy and test as a proof-of-concept."
+*/
 
+const sqlite3=require('sqlite3').verbose()
+const dbName='myDatabse.db'
+
+let db=new sqlite3.Database(dbName,(err)=>{
+    if(err){
+        console.error(err.message)
+    }
+    {
+        console.log("Connected to the Database")
+        db.run('Create table if not exists itmes.'
+            (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      child_name TEXT NOT NULL,
+      arrival_time TEXT,
+      departure_time TEXT,
+      date TEXT NOT NULL
+    )
+        );
+    }
+});
+module.exports= db
