c46409d312ff4071d7c40061b34457713029832a
Huiseon Yi <heesun0514@gmail.com>
2026-04-19 12:04:27 +0100

    Start an Express Server

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+// server.js create an Express sever :  https://runjs.app/blog/how-to-start-a-node-server
+
+// Impiort the express library that helps to mkae a web server
+import express from "express";
+
+//Use express to create a server. Name it app.
+const app = express();
+
+
+//Set the door number to 3000.
+const port=3000;
+
+
+//When someone visits the main page, run this small function
+app.get('/', (req, res) => {
+
+    //"Send back the message: Hello World!"
+    res.send("Hello World!");
+});
+
+
+// Start the server
+app.listen(port,()=>{
+    console.log("Server is runninng"); // Show in terminal
+    console.log(` Express server runing at http://localhost:${3000}`); // Show address 
+});
