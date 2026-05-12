ab218bbc5ce95dde896496c7eaaf726fc8432c0f
Huiseon Yi <heesun0514@gmail.com>
2026-05-12 22:41:04 +0100

    async function checkin/const child_name, const arrival_time,const date

---
+++ frontend/app.js
@@ -0,0 +1,15 @@
+// API BAse URL
+
+const API_BASE='http://localhost:3000/api/attendance';
+
+
+// ============== 1.CHECK-IN (CREATE) ====================
+
+async function checkin(){
+    const child_name=document.getElementById('childName').value;
+    const arrival_time=document.getElementById('arrivalTime').value;
+    const date=document.getElementById('checkinDate').value;
+
+    
+
+}
