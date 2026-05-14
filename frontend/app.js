b3637a247526e739a7965b42b61990e59e100bc3
Huiseon Yi <heesun0514@gmail.com>
2026-05-14 11:12:23 +0100

    .Edit Attendance Time( UPDATE )

--- frontend/app.js
+++ frontend/app.js
@@ -48,7 +48,7 @@ async function checkin(){
 async function checkout(){
 
      //1. grab the values from the input boxes 
-    const id =document.getElementById('checkoutid').value; // ✅ fixed: 'checkoutId'
+    const id =document.getElementById('checkoutId').value; // ✅ fixed: 'checkoutId'
     const department_time =document.getElementById('departureTime').value;
     
 
@@ -83,12 +83,50 @@ async function checkout(){
      } catch(error){ // the internet failed or the server is turned off 
         document.getElementById('checkoutResult').innerHTML=` ❌ Connection error: ${error.message}`;
 
+}
+}
 
+// ============== 3.Edit Attendance Time( UPDATE ) ====================
+
+async function editAttendanceTime(){
+
+     //1. grab the values from the input boxes 
+    const id =document.getElementById('checkinId').value; 
+    const arrival_time =document.getElementById('arrivaleTime').value;
+    const department_time=document.getElementById('departureTime').value;
+    const date=document.getElementById('checkinDate').value;
+    
+
+    //2. simple check : if any box is empty, stop and tell the user 
+    if (!child_name && ! arrival_time && !date){
+        alert('Please enter at least one field')
+        return;
+    }
+
+    try { 
+        //3. send teh "Package " (JSON) to the server 
+          
+        const response = await fetch (`http://localhost:3000/api/attendance/checkin/${id}`,{
+        method:'PUT', // update the data
+        headers:{'Content-type':'application/json'}, // JSOM format 
+        body:JSON.stringify({child_name,arrival_time,department_time,date}) //converts the javascript object into a JSON string
+    });
+
+     //4. open the response from the server
+     const result =await response.json(); // converts the server's response back into a JS object 
      
 
+        
+     if (response.status===200){
+        // sucess ! show a green checkmark and the name 
+        document.getElementById('editAttendanceTimeResult').innerHTML=`✅ Attendance record updated sucessfully! record ID:${id} at ${arrival_time},${department_time},${date}`;
+        loadTodayAttendance(); // Update the list 
 
-
-
+        } else { // the server said no (e.g.,missing data )
+            document.getElementById('editAttendanceTimeResult').innerHTML=`❌ Error:${result.error} `;
+        }
+     } catch(error){ // the internet failed or the server is turned off 
+        document.getElementById('editAttendanceTimeResult').innerHTML=` ❌ Connection error: ${error.message}`;
 
 }
 }
